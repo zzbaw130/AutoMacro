f40fb5c8a41cc14c3c74cc548ec71af5a8759fca
zzbaw130 <2431771156@qq.com>
2026-04-15 17:47:32 +0800

    v1.4.1 增加注释, 修复子模块导入错误, 支持多窗口切换以及默认全屏

--- Macro.py
+++ Macro.py
@@ -1,23 +1,27 @@
-from ctypes import windll
-from pathlib import Path
-from typing import TypedDict, Unpack
+# 当AutoMacro被当作子模块时, onnxocr是子模块的子模块, 需要通过相对路径导入
+try:
+    from onnxocr.onnx_paddleocr import ONNXPaddleOcr, sav2Img
+except:
+    from .onnxocr.onnx_paddleocr import ONNXPaddleOcr, sav2Img
 
+from pathlib import Path
 import cv2
+from types import SimpleNamespace
 import pygetwindow as gw
 import time
-import random
 import dxcam
-import win32gui
+import win32gui, win32api
 from pynput.keyboard import Controller as keyCtrl, Key
 from pynput.mouse import Controller as mouseCtrl, Button
 import random
-from .onnxocr.onnx_paddleocr import ONNXPaddleOcr, sav2Img
+import logging
+import numpy as np
 
-
-# windll.user32.SetProcessDPIAware()
-class ActionParams(TypedDict, total=False):
-    pre_delay: float
-    post_delay: float
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s] [%(levelname)s] %(message)s",
+    datefmt="%a %d %b %Y %H:%M:%S",
+)
 
 
 BTN_MAPPING = {
@@ -90,7 +94,17 @@ KB_MAPPING = {
 }
 
 
-def get_window_client_rect(hwnd) -> tuple[int, int, int, int]:
+def get_window_client_rect(hwnd: int) -> tuple[int, int, int, int]:
+    """
+    获取窗口客户区矩形坐标
+    参数:
+        hwnd: windows窗体句柄
+    返回:
+        形如 [左, 上, 右, 下] 的四个边界值
+    """
+    # 句柄为全屏
+    if hwnd == 0:
+        return 0, 0, win32api.GetSystemMetrics(0), win32api.GetSystemMetrics(1)
     # 获取客户区矩形
     client_rect = win32gui.GetClientRect(hwnd)
     # 转换为屏幕坐标
@@ -101,56 +115,109 @@ def get_window_client_rect(hwnd) -> tuple[int, int, int, int]:
 
 
 def isListEmpty(alist: list) -> bool:
+    """
+    判断List内部是否为空, 支持空字符串, 空列表嵌套等情况
+    参数:
+        alist: 待判断的List
+    返回:
+        为空则True, 否则False
+    """
     if isinstance(alist, list):
         return all(map(isListEmpty, alist))
     return False
 
 
 class Macro:
-    def __init__(self, title: str):
-        self.title: str = title
-        # 将指定窗口放置最前
-        self.window: gw.Win32Window = gw.getWindowsWithTitle(title)[0]
-        l, t, r, b = get_window_client_rect(self.window._hWnd)
-        self.resolution = (r - l, b - t)
-        self.switchToWindow()
+    def __init__(self, title: str = None, index: int = 0):
+        """
+        初始化Macro, 一个Macro用于操控一系列同名的进程, 利用index指定默认操作的进程
+        参数:
+            title: 进程标题名, 支持模糊搜索, 若不填写, 则默认选取整个屏幕
+            index: 若存在多个同名的进程, 则可通过index指定具体使用哪个
+        """
+        if title:
+            self.title: str = title
+            # 将指定窗口放置最前
+            self.windows: list[gw.Win32Window] = gw.getWindowsWithTitle(title)
+            self.cur_index: int = index
+            self.last_index: int = -1
+            l, t, r, b = get_window_client_rect(self.windows[index]._hWnd)
+            self.resolution: tuple[int, int] = (r - l, b - t)
+        # 若未指定窗口, 则指定全屏
+        else:
+            self.title: str = "screen"
+            # 创建一个简单的对象用以后续访问
+            self.windows: list[gw.Win32Window] = [SimpleNamespace(**{"isActive": True, "_hWnd": 0})]
+            self.cur_index: int = -1
+            self.last_index: int = -1
+            self.resolution: tuple[int, int] = (win32api.GetSystemMetrics(0), win32api.GetSystemMetrics(1))
+
         self.keyboard = keyCtrl()
         self.mouse = mouseCtrl()
         self._cam = dxcam.create(output_color="BGR")
         self._ocr_handler = ONNXPaddleOcr(use_angle_cls=False, use_gpu=False)
         self._template_cache = {}
 
-    def random_delay(self, sec:int, float_range:float):
+    def random_delay(self, sec: int, float_range: float = 0.1):
         """
         随机延迟
-
-        :param ms: 指定延迟的ms
-        :param float_range: 在ms的基础上偏移指定范围, 默认偏移百分之十(0.1)
+        参数:
+            sec: 指定延迟的秒数
+            float_range: 在指定秒数的基础上偏移指定范围, 默认偏移百分之十(0.1)
         """
         ms = sec * 1000
         random_delay_in_ms = random.randint(int(ms * (1 - float_range)), int(ms * (1 + float_range)))
         time.sleep(random_delay_in_ms / 1000)
 
-    def switchToWindow(self):
-        if self.window.isActive:
+    def switchToWindow(self, index: int = -1):
+        """
+        切换到指定的窗口, 可通过index指定切换到哪个窗口, 若不指定, 则激活初始化窗口
+        参数:
+            index: 窗口index
+        """
+        target_index = self.cur_index if index < 0 else index
+        target_window = self.windows[target_index]
+        logging.debug(f"目标窗口: {target_index}, 当前窗口: {self.cur_index}")
+        # 窗口已经激活
+        if target_window.isActive:
+            logging.debug(f"窗口已激活, 无需切换")
             return
-        # 如果窗口最小化的话, 将其激活也无法放置最前, 因此需要先恢复
-        if self.window.isMinimized:
-            self.window.restore()
-        self.window.activate()
+        # 第一次启动或激活自己可以使用activate
+        if self.last_index == -1 or self.last_index == target_index:
+            # 如果窗口最小化的话, 将其激活也无法放置最前, 因此需要先恢复
+            if target_window.isMinimized:
+                target_window.restore()
+            target_window.activate()
+            logging.debug(f"由主屏幕切换或激活自身, 使用active方法")
+        # 从已经激活过的应用切换activate会报错, 使用缩小放大法
+        else:
+            target_window.minimize()
+            target_window.restore()
+            self.cur_index = target_index
+            logging.debug(f"由另一窗口切换, 使用restore方法")
+
+        self.last_index = target_index
         # 切换窗口有动画, 需要等待
         self.random_delay(0.5)
 
     def grab(
         self,
-        roi: tuple[int, int, int, int] = (0, 0, 0, 0),
+        roi: tuple[int, int, int, int] = None,
         save_path: Path | str = None,
-    ):
+    ) -> np.ndarray[np.uint8]:
+        """
+        截取指定roi区域的图片
+        参数:
+            roi: tuple[int, int, int, int], 形如[x, y, width, height]的roi区域
+            save_path: Path | str, 截图保存路径, 若为None, 则不保存
+        返回:
+            截图图片的矩阵, 可用于后续OCR识别
+        """
         # self.switchToWindow()
-        rect = get_window_client_rect(self.window._hWnd)
+        rect = get_window_client_rect(self.windows[self.cur_index]._hWnd)
 
         # 指定ROI区域 x,y,w,h
-        if roi != (0, 0, 0, 0):
+        if roi is not None:
             x, y, w, h = roi
             cl, ct, _, _ = rect
             rect = (cl + x, ct + y, cl + x + w, ct + y + h)
@@ -174,15 +241,14 @@ class Macro:
     ) -> tuple[bool, tuple[int, int], float]:
         """
         查找图像
-
-        :param self: Description
-        :param template_path: 模板图片路径,
-            实际存储的模板图片命名格式为resolution-x_y_w_h-name.png, 其中resolution为当前应用分辨率,
-            x,y,w,h为ROI, name为图片名称, 例如1920x1080-100_200_300_400-start_button.png
-            正式使用时, 只需要传入name.png部分即可, 例如start_button.png, 程序会根据当前应用分辨率自动寻找对应的模板图片
-        :param roi: 查找范围, 格式为(x, y, w, h), 若设置了此参数, 则会覆盖模板图片命名中的roi参数
-        :param threshold: 匹配阈值, 默认0.8
-        :return: 返回值形如(是否匹配成功, 在应用界面的中心坐标, 模板的roi)
+        参数:
+            template_path: 模板图片路径,实际存储的模板图片命名格式为 resolution-x_y_w_h-name.png,
+                其中resolution为当前应用分辨率, x,y,w,h为ROI, name为图片名称, 例如1920x1080-100_200_300_400-start_button.png
+                正式使用时, 只需要传入name.png部分即可, 例如start_button.png, 程序会根据当前应用分辨率自动寻找对应的模板图片
+            roi: 查找范围, 格式为(x, y, w, h), 若设置了此参数, 则会覆盖模板图片命名中的roi参数
+            threshold: 匹配阈值, 默认0.8
+        返回:
+            返回值形如 (Ture, (10, 10), (0, 0, 1920, 1080)), 参数意义为(是否匹配成功, 在应用界面的中心坐标, 模板的roi)
         """
         # 寻找模板图片
         tp = Path(template_path)
@@ -198,7 +264,7 @@ class Macro:
                 try:
                     roi = tuple([int(i) for i in template_name.split("-")[-1].split("_")])
                 except ValueError:
-                    roi = (0, 0, 0, 0)
+                    roi = None
             # 根据路径缓存图片
             self._template_cache.update({str(template_path): (template, roi)})
         else:
@@ -220,14 +286,24 @@ class Macro:
             return True, (center_x, center_y), template_roi
         return False, None, None
 
-    def ocr(self, image_or_roi: Path | str | tuple[int, int, int, int]):
-        if type(image_or_roi) is tuple:
+    def ocr(self, image_or_roi: Path | str | tuple[int, int, int, int] = None, saveResult: bool = False):
+        """
+        OCR识别文字
+        参数:
+            image_or_roi: 可以传入现有的图片路径, 也可以指定需要识别的ROI区域
+        返回:
+            识别到的文字
+        """
+        if image_or_roi is None or type(image_or_roi) is tuple:
             img = self.grab(roi=image_or_roi)
         else:
             img = cv2.imread(Path(image_or_roi))
 
         # 只ocr一行, 最终结果一定为单个
         res = self._ocr_handler.ocr(img)
+        if saveResult:
+            sav2Img(img, res)
+
         # 结果为空
         if isListEmpty(res):
             return ""
@@ -236,12 +312,22 @@ class Macro:
         ocr_text = box[1][0]
         return ocr_text
 
-    def click(self, x, y, clicks: int = 1, interval: float = 0.01, button: str = "left", duration: float = 0.01):
-        cl, ct, _, _ = get_window_client_rect(self.window._hWnd)
+    def click(self, x, y, clicks: int = 1, interval: float = 0.1, button: str = "left", duration: float = 0.1):
+        """
+        鼠标点击
+        参数:
+            x: 点击的x坐标(相对于客户区)
+            y: 点击的y坐标(相对于客户区)
+            clicks: 点击次数
+            interval: 点击间隔
+            button: "left", "middle", "right" 分别代表鼠标左键、中键、右键
+            duration: 按下鼠标后的持续时间, 然后松开, 视为一次点击
+        """
+        cl, ct, _, _ = get_window_client_rect(self.windows[self.cur_index]._hWnd)
         absolute_x = x + cl
         absolute_y = y + ct
         self.mouse.position = (absolute_x, absolute_y)
-        time.sleep(0.01)
+        time.sleep(0.02)
         for i in range(clicks):
             self.mouse.press(BTN_MAPPING[button])
             time.sleep(duration)
@@ -257,6 +343,15 @@ class Macro:
         button: str = "left",
         duration: float = 0.0,
     ):
+        """
+        随机点击指定ROI区域中的一个位置
+        参数:
+            roi: 指定ROI区域
+            clicks: 点击次数
+            interval: 点击间隔
+            button: "left", "middle", "right" 分别代表鼠标左键、中键、右键
+            duration: 按下鼠标后的持续时间, 然后松开, 视为一次点击
+        """
         l, t, w, h = roi
 
         random_x = random.randint(l, l + w)
@@ -264,13 +359,33 @@ class Macro:
         self.click(random_x, random_y, clicks, interval, button, duration)
 
     def wheel_up(self, unit: int = 1):
+        """
+        鼠标滚轮向上滚动
+        参数:
+            unit: 滚动的格数
+        """
         self.mouse.scroll(0, unit)
 
     def wheel_down(self, unit: int = 1):
+        """
+        鼠标滚轮向下滚动
+        参数:
+            unit: 滚动的格数
+        """
         self.mouse.scroll(0, -unit)
 
     def drag(self, x1: int, y1: int, x2: int, y2: int, duration: float, button: str = "left"):
-        cl, ct, _, _ = get_window_client_rect(self.window._hWnd)
+        """
+        鼠标拖拽
+        参数:
+            x1: 起始点x坐标
+            y1: 起始点y坐标
+            x2: 终止点x坐标
+            y2: 终止点y坐标
+            duration: 模拟拖拽时间(按照60帧来进行模拟)
+            button: "left", "middle", "right" 分别代表鼠标左键、中键、右键
+        """
+        cl, ct, _, _ = get_window_client_rect(self.windows[self.cur_index]._hWnd)
         absolute_x1 = x1 + cl
         absolute_y1 = y1 + ct
 
@@ -290,19 +405,53 @@ class Macro:
         self.mouse.release(BTN_MAPPING[button])
 
     def key_down(self, key: str):
+        """
+        键盘按下
+        参数:
+            key: 单个按键, 请参照KB_MAPPING
+        """
         self.keyboard.press(key if len(key) == 1 else KB_MAPPING[key])
         time.sleep(0.01)
 
     def key_up(self, key: str):
+        """
+        键盘抬起
+        参数:
+            key: 单个按键, 请参照KB_MAPPING
+        """
         self.keyboard.release(key if len(key) == 1 else KB_MAPPING[key])
         time.sleep(0.01)
 
-    def key_press(self, keys: str | list, duration: float = 0.01, interval: float = 0.01):
-        keys = list(keys)
-        for i, key in enumerate(keys):
-            m_key = key if len(key) == 1 else KB_MAPPING[key]
-            self.keyboard.press(m_key)
-            time.sleep(duration)
-            self.keyboard.release(m_key)
-            if i < len(keys) - 1:
-                time.sleep(interval)
+    def key_press(self, keys: str | list, duration: float = 0.1, interval: float = 0.1):
+        """
+        敲击键盘
+        参数:
+            keys: 需要键入的按键, 若为
+            duration:
+            interval:
+        """
+
+        # 用于处理字符串形式输入的key
+        def press_key_str(key_str: str, duration: int, interval: int):
+            # 若输入的字符串匹配上功能键, 则直接键入功能键
+            if key_str in KB_MAPPING:
+                self.keyboard.press(KB_MAPPING[key_str])
+                time.sleep(duration)
+                self.keyboard.release(KB_MAPPING[key_str])
+            else:
+                single_key_list = list(key_str)
+                for i, sk in enumerate(single_key_list):
+                    self.keyboard.press(sk)
+                    time.sleep(duration)
+                    self.keyboard.release(sk)
+                    if i < len(single_key_list) - 1:
+                        time.sleep(interval)
+
+        # 用户输入的是一个字符串, 直接运行
+        if type(keys) == str:
+            press_key_str(keys, duration, interval)
+        # 用户输入的是一个字符串列表, 分解为多个字符串依次运行
+        else:
+            keys_list = list(keys)
+            for key_str in keys_list:
+                press_key_str(key_str, duration, interval)
